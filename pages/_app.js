f3d2282504b5a72aae922eab631fbb2c7a9c387c
Muhammad Hamza Mehdi <135020255+HamzaMehdi12@users.noreply.github.com>
2025-05-21 21:22:06 +0900

    Delete pages directory

--- pages/_app.js
+++
@@ -1,1 +0,0 @@
-import '../styles/globals.css'; import Header from '../components/Header'; export default function MyApp({ Component, pageProps }) { return (<><Header /><Component {...pageProps} /></>); }
